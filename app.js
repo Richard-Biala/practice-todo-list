6010544a2e6873ac3f8c92eb3988e6c311cbfc47
Richard <rkj128@gmail.com>
2020-10-10 07:56:49 -0700

    Selector update

---
+++ app.js
@@ -0,0 +1,9 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todolist = document.querySelector('.todo-list');
+
+
+//Event Listeners
+
+//Functions
