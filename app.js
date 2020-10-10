e6676eeec0e5ef9da72dc53637fc8d4c2b8acf2e
Richard <rkj128@gmail.com>
2020-10-10 08:56:37 -0700

    addTodo event completed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Selectors
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
-const todolist = document.querySelector('.todo-list');
+const todoList = document.querySelector('.todo-list');
 
 
 //Event Listeners
@@ -33,6 +33,9 @@ function addTodo(event){
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
     trashButton.classList.add('completed-btn');
     todoDiv.appendChild(trashButton);
+
+    //Append to list
+    todoList.appendChild(todoDiv);
 
 
 }
