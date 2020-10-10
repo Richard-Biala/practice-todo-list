19cb91d45a02b5a92da0ec0fa566b13640b7ff24
Richard <rkj128@gmail.com>
2020-10-10 14:58:17 -0700

    delete animation updates

--- app.js
+++ app.js
@@ -50,6 +50,16 @@ function deleteCheck(e){
     //Delete todo
     if(item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
-        todo.remove();
+        //Animation
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function (){
+            todo.remove();
+        })
+    }
+
+    //Check Mark
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
     }
 }
